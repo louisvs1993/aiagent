ee778064a6fe3eb3b1b58754c77069cf43e9b111
louisvs1993 <louisvansteelant@hotmail.com>
2025-08-27 17:24:15 +0200

    verbose argument option added

--- main.py
+++ main.py
@@ -2,25 +2,49 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
+from google.genai import types
 
 
 
 def main():
     load_dotenv()
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        sys.exit(1)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    if len(sys.argv) > 1:
-        generated_content = client.models.generate_content(
-            model="gemini-2.0-flash-001", 
-            contents=sys.argv[1]
-            )
-        print("Prompt tokens:", generated_content.usage_metadata.prompt_token_count)
-        print("Response tokens:", generated_content.usage_metadata.candidates_token_count)
+
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
+    generated_content = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    user_prompt = sys.argv[1]
+    prompt_tokens = generated_content.usage_metadata.prompt_token_count
+    response_tokens = generated_content.usage_metadata.candidates_token_count
+
+    if "--verbose" in sys.argv or "-v" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
         print("Response:")
         print(generated_content.text)
     else:
-        raise Exception("No argument given")
-
+        print(generated_content.text)
 
 if __name__ == "__main__":
     main()
