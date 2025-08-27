a2964562d95f7b0c783f9b0e54b889a69420bec0
louisvs1993 <louisvansteelant@hotmail.com>
2025-08-27 16:58:37 +0200

    hardcoded prompt replaced by input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 
 
@@ -8,14 +9,17 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    generated_content = client.models.generate_content(
-        model="gemini-2.0-flash-001", 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-        )
-    print("Prompt tokens:", generated_content.usage_metadata.prompt_token_count)
-    print("Response tokens:", generated_content.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(generated_content.text)
+    if len(sys.argv) > 1:
+        generated_content = client.models.generate_content(
+            model="gemini-2.0-flash-001", 
+            contents=sys.argv[1]
+            )
+        print("Prompt tokens:", generated_content.usage_metadata.prompt_token_count)
+        print("Response tokens:", generated_content.usage_metadata.candidates_token_count)
+        print("Response:")
+        print(generated_content.text)
+    else:
+        raise Exception("No argument given")
 
 
 if __name__ == "__main__":
